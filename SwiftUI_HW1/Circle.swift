f3b87f2ba7fd87260d9ac0c5669ff4fe8b0bb6cf
GenerallissimoF <95148081+GenerallissimoF@users.noreply.github.com>
2022-02-16 21:30:20 +0300

    final commit

---
+++ SwiftUI_HW1/Circle.swift
@@ -0,0 +1,26 @@
+//
+//  Circle.swift
+//  SwiftUI_HW1
+//
+//  Created by Ivan Adoniev on 15.02.2022.
+//
+
+import SwiftUI
+
+struct ColorCircle: View {
+    let color: Color
+    
+    var body: some View {
+        Circle()
+            .foregroundColor(color)
+            .frame(width: 100, height: 100)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct ColorCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCircle(color: .blue)
+    }
+}
--- SwiftUI_HW1/ContentView.swift
+++ SwiftUI_HW1/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var buttonTitle = "Start"
+    @State var nextColorState: TrafficLights = .red
+    
+    enum TrafficLights {
+        case red
+        case yellow
+        case green
+    }
+   
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+        VStack {
+            switchTheLight()
+        }
+        VStack {
+            Button(action: { changeLights() }) {
+                Text(buttonTitle)
+                    .foregroundColor(.white)
+            }
+        }
+        .padding()
+        .frame(width: 200, height: 40, alignment: .center)
+        .background(Color.indigo)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+    }
+    
+    func changeLights() {
+     
+        buttonTitle = "Next"
+        
+        switch nextColorState {
+        case .red:
+            nextColorState = .yellow
+        case .yellow:
+            nextColorState = .green
+        case .green:
+            nextColorState = .red
+        }
     }
 }
 
@@ -19,3 +58,37 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+extension ContentView {
+    
+    func switchTheLight() -> some View {
+        Group {
+            switch nextColorState {
+            case .red:
+                ColorCircle(color: .red)
+                    .opacity(1.0)
+                ColorCircle(color: .yellow)
+                    .opacity(0.1)
+                ColorCircle(color: .green)
+                    .opacity(0.1)
+                    .padding(.bottom, 40)
+            case .yellow:
+                ColorCircle(color: .red)
+                    .opacity(0.1)
+                ColorCircle(color: .yellow)
+                    .opacity(1.0)
+                ColorCircle(color: .green)
+                    .opacity(0.1)
+                    .padding(.bottom, 40)
+            case .green:
+                ColorCircle(color: .red)
+                    .opacity(0.1)
+                ColorCircle(color: .yellow)
+                    .opacity(0.1)
+                ColorCircle(color: .green)
+                    .opacity(1.0)
+                    .padding(.bottom, 40)
+            }
+        }
+    }
+}
+
